820af913e3fe52d7537a5fb8f66db7bc685339b0
Sonja Lechner <blaquekaktus@gmail.com>
2024-06-25 15:24:29 +0000

    I/O

--- src/main/java/_06_02/before/ScannerExample.java
+++ src/main/java/_06_02/before/ScannerExample.java
@@ -1,8 +1,31 @@
 package _06_02.before;
 
+import java.util.Scanner;
+
 public class ScannerExample {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Your name is " + name);
+
+        System.out.print("Enter your age: ");
+        int age = scan.nextInt();
+        int yob = 2024 - age;
+        System.out.print(name + " were you born in " + yob + "? Type 1 for yes or 0 for no: ");
+        int answer = scan.nextInt();
+        if (answer == 0){
+             yob = 2023 - age;
+            System.out.print(name + " were you born in " + yob + "? Type 1 for yes or 0 for no: ");
+            answer = scan.nextInt();
+        }else{
+            System.out.println("I knew it!");
+        }
+
+        
+
+
 
     }
 
