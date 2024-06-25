47ac77b11ae982f41f4be0458e7d17fc92a3adb4
Sonja Lechner <blaquekaktus@gmail.com>
2024-06-25 14:34:20 +0000

    Concurrency

--- src/main/java/_05_02/before/Main.java
+++ src/main/java/_05_02/before/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Thread thread1 = new ThreadExample();   
+        Thread thread2 = new ThreadExample(); 
+        Thread thread3 = new ThreadExample(); 
+
+        thread1.setName("thread1");
+        thread2.setName("thread2");
+        thread3.setName("thread3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
     }
 
 }
