e10627a1b0ef1a79d9d0a462b7e61d25c28b7c93
Sonja Lechner <blaquekaktus@gmail.com>
2024-06-26 07:26:34 +0000

    *

--- src/main/java/_06_02/before/ScannerExample.java
+++ src/main/java/_06_02/before/ScannerExample.java
@@ -17,7 +17,7 @@ public class ScannerExample {
         int answer = scan.nextInt();
         if (answer == 0){
              yob = 2023 - age;
-             System.out.print(name + " were you born in " + yob + "!");
+            System.out.print(name + " you were born in " + yob + "!");
             answer = scan.nextInt();
         }else{
             System.out.println("I knew it!");
